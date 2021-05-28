346d1184e9e0e6156fcebe167507e007262c8357
Owen Brice <brice@sowellapp.com>
2021-05-28 14:59:09 +0300

    #1

--- ui/src/components/Component.js
+++ ui/src/components/Component.js
@@ -1,4 +1,4 @@
-import { h } from "vue";
+import { h, ref } from 'vue'
 import {
   QPage,
   QCard,
@@ -7,62 +7,71 @@ import {
   QInput,
   QCardActions,
   QBtn,
-} from "quasar";
-import { ref } from "vue";
-import axios from "axios";
+} from 'quasar'
+import { ref, reactive } from 'vue'
+import axios from 'axios'
 
 export default {
-  name: "SowellAuth",
+  name: 'SowellAuth',
 
   props: {
     store: Object,
   },
 
   setup({ store }) {
-    const email = ref("");
-    const password = ref("");
+    const email = ref('')
+    const password = ref('')
 
-    const submitHandler = async () => {
-      console.log("submit....");
+    const validations = {
+      email: [
+        (val) => !!val || "L'adresse email doit être fourni",
+        (val) => isEmail || 'Email non valide',
+      ],
+      password: [(val) => !!val || 'Le mot de passe doit être fourni'],
+    }
 
-      const data = {
-        auth: { email: "gardienb@sowellapp.com", password: "123456" },
-      };
+    const submitHandler = () => {
+      console.log('email....', email.value)
+      console.log('password....', password.value)
 
-      var response = "";
+      /*const data = {
+        auth: { email: 'gardienb@sowellapp.com', password: '123456' },
+      }
+
+      var response = ''
       try {
-        response = await axios.post("https://api.sowellapp.com/tokens", data);
-        if (response.status == "201") {
-          console.log(response);
-          store.commit("auth/setJWT", response.data.jwt);
+        response = await axios.post('https://api.sowellapp.com/tokens', data)
+        if (response.status == '201') {
+          console.log(response)
+          store.commit('auth/setJWT', response.data.jwt)
         } else {
-          console.log("Not 201");
-          store.commit("auth/clearJWT");
+          console.log('Not 201')
+          store.commit('auth/clearJWT')
         }
       } catch (err) {
-        console.log("Error...");
-        store.commit("auth/clearJWT");
-      }
-    };
+        console.log('Error...')
+        store.commit('auth/clearJWT')
+      }*/
+    }
 
     return () => [
       h(
         QPage,
         {
-          class: "flex flex-center",
+          class: 'flex flex-center',
         },
         [
           h(
             QCard,
             {
-              class: "card",
+              class: 'card',
             },
             [
               h(QCardSection, [
                 h(
-                  "div",
+                  'div',
                   {
-                    class: "text-6 text-center title",
+                    class: 'text-6 text-center title',
                   },
                   "Vos identifiants d'accès"
                 ),
@@ -70,39 +79,47 @@ export default {
               h(
                 QForm,
                 {
-                  class: "q-gutter-md",
+                  class: 'q-gutter-md',
                   submit: submitHandler,
                 },
                 [
                   h(QCardSection, [
                     h(QInput, {
-                      class: "full-width q-mb-md",
+                      class: 'full-width q-mb-md',
                       outlined: true,
                       rounded: true,
                       dense: true,
-                      label: "Email",
-                      type: "email",
+                      label: 'Email',
+                      props: {
+                        'v-model': email.value,
+                      },
+                      type: 'email',
                       modelValue: email,
                     }),
                     h(QInput, {
-                      class: "full-width q-mb-md",
+                      class: 'full-width q-mb-md',
                       outlined: true,
                       rounded: true,
                       dense: true,
-                      label: "Mot de passe",
-                      type: "password",
+                      label: 'Mot de passe',
+                      type: 'password',
                       modelValue: password,
+                      props: {
+                        'v-model': password.value,
+                      },
+                      rules: [validations['password']],
                     }),
                   ]),
                   h(
                     QCardActions,
                     h(QBtn, {
-                      label: "Connexion",
-                      class: "full-width",
+                      label: 'Connexion',
+                      class: 'full-width',
                       rounded: true,
                       dense: true,
-                      color: "primary",
-                      type: "submit",
+                      color: 'primary',
+                      type: 'submit',
+                      onClick: (event) => submitHandler(),
                     })
                   ),
                 ]
@@ -111,6 +128,6 @@ export default {
           ),
         ]
       ),
-    ];
+    ]
   },
-};
+}
